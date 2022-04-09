e84c128f7e83df803596fc8e382cea9944feb6fb
Gabriela <31055989+gabrielaokubara@users.noreply.github.com>
2022-04-08 21:26:55 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,25 +7,25 @@ import imgMaria from './images/maria.jpg'
 import imgJaqueline from './images/jaqueline.jpg'
 
 const AppExercicio = () => {
-    const estiloFundo = { marginTop: 12, paddingTop: '5%', paddingBottom: '2%' ,backgroundColor: 'lightGray', width: '100%', borderRadius: 8 }
+    const backgroundStyle = { marginTop: 12, paddingTop: '5%', paddingBottom: '2%' ,backgroundColor: 'lightGray', width: '100%', borderRadius: 8 }
     const imageTextSize = { fontSize: 12, textAlign: 'center' }
     const myCardStyle = { display: 'inline-block', marginTop: 12, paddingTop: '1%', padding: 5 , backgroundColor: 'thistle', height: 150, width: 120, borderRadius: 8 }
     const imageStyle = { height: 100, width: 120, borderRadius: 8 }
-    const meuCard = (image, text) => (
+    const myCard = (image, text) => (
         <div className='card' style={myCardStyle}>
             <img src={image} style={imageStyle}></img>
             <p style={imageTextSize}>{text}</p>
         </div>
         )
         
-    const estiloTitulo = { verticalAlign : 'center', textAlign : 'center' }
+    const titleStyle = { verticalAlign : 'center', textAlign : 'center' }
     return (
-        <div style={estiloFundo}>
-            <p style={estiloTitulo}>Profissionais da saúde</p>
+        <div style={backgroundStyle}>
+            <p style={titleStyle}>Profissionais da saúde</p>
             <div style={{display: 'flex', justifyContent:'space-around', margin : 10, padding: 10, paddingBottom: '2%' , borderColor : 'gray', borderStyle : 'solid', borderRadius : 5 }}>
-                {meuCard(imgJose, 'José da Silva')}
-                {meuCard(imgMaria, 'Maria da Silva')}
-                {meuCard(imgJaqueline, 'Jaqueline Mendes')}
+                {myCard(imgJose, 'José da Silva')}
+                {myCard(imgMaria, 'Maria da Silva')}
+                {myCard(imgJaqueline, 'Jaqueline Mendes')}
             </div>
         </div>
     )
